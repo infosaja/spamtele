b73504bff6d57a6456b005245e4526b8ef3b391d
infosaja <164633135+infosaja@users.noreply.github.com>
2024-03-23 19:39:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function sendMessage() {
     var botToken = document.getElementById("botToken").value;
     var message = document.getElementById("message").value;
 
-    // Kirim pesan ke Telegram sebanyak 4 kali menggunakan loop
-    for (var i = 0; i < 9; i++) {
-        // Buat objek XMLHttpRequest untuk setiap pengiriman pesan
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + telegramId + "&text=" + encodeURIComponent(message), true);
-        xhr.send();
+    // Mengirim pesan ke Telegram sebanyak 4 kali dengan jeda 1 detik
+    for (var i = 0; i < 4; i++) {
+        setTimeout(function() {
+            // Buat objek XMLHttpRequest untuk setiap pengiriman pesan
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://api.telegram.org/bot" + botToken + "/sendMessage?chat_id=" + telegramId + "&text=" + encodeURIComponent(message), true);
+            xhr.send();
+        }, i * 1000); // Mengatur jeda waktu 1 detik (1000 milidetik)
     }
 }
